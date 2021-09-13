8f7574bffc88f9f6a7b20db2470b37486cea9c80
SooYoungJang <tturrr10@gmail.com>
2021-09-13 12:38:19 +0900

    detailFragment add

--- app/src/main/java/com/example/glowdayzapp/presentaion/view/detail/DetailFragment.kt
+++ app/src/main/java/com/example/glowdayzapp/presentaion/view/detail/DetailFragment.kt
@@ -1,13 +1,31 @@
 package com.example.glowdayzapp.presentaion.view.detail
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.glowdayzapp.R
+import com.example.glowdayzapp.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+    val args: DetailFragmentArgs by navArgs()
+    private lateinit var binding: FragmentDetailBinding
+    private lateinit var callback: OnBackPressedCallback
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +35,38 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailBinding.bind(view)
+
+        initView()
+    }
+
+
+
+    fun initView() {
+
+        binding.imgBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        if (args.recommendProduct == null && args.product != null) {
+            Glide.with(requireActivity())
+                .load(args.product?.imageUrl)
+                .error(R.drawable.ic_launcher_background)
+                .into(binding.productImage)
+
+            binding.productTitle.text = args.product?.productTitle
+        } else if (args.recommendProduct != null && args.product == null) {
+            Glide.with(requireActivity())
+                .load(args.recommendProduct?.imageUrl)
+                .error(R.drawable.ic_launcher_background)
+                .into(binding.productImage)
+
+            binding.productTitle.text = args.recommendProduct?.productTitle
+        }
+
+
+    }
 
 }
